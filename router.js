6709d6b04e6e5ee91c7bf3e31ac79fde9d385440
Michael Efemena <mikkybang@localhost.localdomain>
2019-05-27 12:17:03 +0100

    send grid

--- router.js
+++ router.js
@@ -1,68 +1,63 @@
 var express = require("express");
 var router = express.Router();
-var User = require("./User")
+var User = require("./User");
 const rateLimit = require("express-rate-limit");
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
+var sgTransport = require('nodemailer-sendgrid-transport');
 
-var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'mefemena@gmail.com',
-      pass: '123daddy'
-    },
-    tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false
-    }
-  });
+var options = {
+  auth: {
+    api_user: 'mikkybang',
+    api_key: '123mikkybang'
+  }
+}
 
+var transporter = nodemailer.createTransport(sgTransport(options));
 
 const loginLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 2, // start blocking after 3 requests,
-    onLimitReached:  function (req, res, next) {
-        var mailOptions = {
-            from: 'mefemenagmail.com',
-            to: req.body.email,
-            subject: 'Suspicious Account Activity',
-            text: 'Someone Has tried to login into your account with incorrect Credentials more than three times'
-        };
-        
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-            console.log(error);
-            } else {
-            console.log('Email sent: ' + info.response);
-            }
-        });
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 3, // start blocking after 3 requests,
+  onLimitReached: function(req, res, next) {
+    var mailOptions = {
+      from: "mefemena@gmail.com",
+      to: req.body.email,
+      subject: "Suspicious Account Activity",
+      text:
+        "Someone Has tried to login into your account with incorrect Credentials more than three times"
+    };
 
-        res.status(404).send("Too many Login attempts from this IP, please try again after 5 Minutes")
-    },
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
 
-
-   
-
-router.post("/register", async (req, res) => {
-    const user = await (new User(req.body).save());
-    res.json(user);
+    res
+      .status(404)
+      .send(
+        "Too many Login attempts from this IP, please try again after 5 Minutes"
+      );
+  }
 });
 
-router.post("/login", loginLimiter ,async (req, res) => {
-    const user = await User.findOne({email: req.body.email})
-    if(!user){
-        res.status(404).send("User not Found")
+router.post("/register", async (req, res) => {
+  const user = await new User(req.body).save();
+  res.json(user);
+});
+
+router.post("/login", loginLimiter, async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    res.status(404).send("User not Found");
+  } else {
+    if (user.password === req.body.password) {
+      res.json(user);
+    } else {
+      res.status(404).send("incorrect password");
     }
-    else{
-        if(user.password === req.body.password){
-            res.json(user)
-        }
-        else{
-            res.status(404).send("incorrect password")
-        }
-    }
-} );
+  }
+});
 
 module.exports = router;
